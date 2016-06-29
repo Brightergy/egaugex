710294c2577aeca82cf5bb255674a3739e48b4a1
techgaun <coolsamar207@gmail.com>
2016-06-29 15:27:43 -0500

    fix issue with matching data

--- lib/egaugex.ex
+++ lib/egaugex.ex
@@ -102,34 +102,40 @@ defmodule Egaugex do
     Parses egauge data body and returns a map of attribute, register and values
     """
     def parse_egauge_data(data) do
-      [{_, atts, values} | _rest] = Floki.find(data, "data")
+      data_block = Floki.find(data, "data")
+      case data_block |> Enum.count do
+        0 ->
+          :error
+        _ ->
+          [{_, atts, values} | _rest] = Floki.find(data, "data")
 
-      # get attributes
-      atts = atts |> Enum.map(fn {name, value} ->
-          case value do
-              "0x" <> num -> {name, hex_to_int(num)}
-              _ -> {name, value}
-          end
-      end) |> Enum.into(%{})
+          # get attributes
+          atts = atts |> Enum.map(fn {name, value} ->
+              case value do
+                  "0x" <> num -> {name, hex_to_int(num)}
+                  _ -> {name, value}
+              end
+          end) |> Enum.into(%{})
 
-      # create list of registers
-      registers = values
-        |> Floki.find("cname")
-        |> Enum.map(fn {_, _, register} -> register end)
-        |> List.flatten
+          # create list of registers
+          registers = values
+            |> Floki.find("cname")
+            |> Enum.map(fn {_, _, register} -> register end)
+            |> List.flatten
 
-      # get values for first register name
-      values = Floki.find(values, "r")
-        |> Enum.map(fn r ->
-          Floki.find(r, "c")
-          |> Enum.map(fn {_, _, value} -> value end)
-          |> List.flatten
-        end)
-        |> List.zip
-        |> Enum.map(fn x -> Tuple.to_list(x) end)
+          # get values for first register name
+          values = Floki.find(values, "r")
+            |> Enum.map(fn r ->
+              Floki.find(r, "c")
+              |> Enum.map(fn {_, _, value} -> value end)
+              |> List.flatten
+            end)
+            |> List.zip
+            |> Enum.map(fn x -> Tuple.to_list(x) end)
 
-      data = Enum.zip(registers, values) |> Enum.into(%{})
-      %{attributes: atts, data: data}
+          data = Enum.zip(registers, values) |> Enum.into(%{})
+          %{attributes: atts, data: data}
+      end
     end
 
     @doc """
